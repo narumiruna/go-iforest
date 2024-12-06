5d3232e1ffd90cca6ba25769701581094b92ab1d
narumi <toucans-cutouts0f@icloud.com>
2024-12-06 23:57:22 +0800

    use gonum

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/narumiruna/go-isolation-forest
 
 go 1.23.2
+
+require gonum.org/v1/gonum v0.15.1
--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 
-	"github.com/narumiruna/go-isolation-forest/pkg/core"
-	"github.com/narumiruna/go-isolation-forest/pkg/types"
+	"github.com/narumiruna/go-isolation-forest/pkg/iforest"
+	"gonum.org/v1/gonum/mat"
 )
+
+func RandomMatrix(n, dim int, bias float64) *mat.Dense {
+	data := make([]float64, n*dim)
+	for i := range data {
+		data[i] = rand.Float64() + bias
+	}
+	return mat.NewDense(n, dim, data)
+}
 
 func main() {
 	dim := 2
-	x := types.RandomMatrix(1000, dim)
-	y := types.RandomMatrix(10, dim).AddScalar(0.5)
+	x := RandomMatrix(1000, dim, 0)
+	y := RandomMatrix(10, dim, 0.5)
 
-	model := core.NewIsolationForest()
+	model := iforest.New()
 	model.Fit(x)
 
 	scores := model.Score(y)
