e28fc386d08f203c202a8f80b1fc69a9ca825cb1
narumi <toucans-cutouts0f@icloud.com>
2024-12-07 22:14:31 +0800

    add quantile

---
+++ pkg/iforest/quantile_test.go
@@ -0,0 +1,25 @@
+package iforest
+
+import (
+	"testing"
+)
+
+func TestQuantile(t *testing.T) {
+	cases := []struct {
+		numbers  []float64
+		q        float64
+		expected float64
+	}{
+		{[]float64{1, 2}, 0.5, 1.5},
+		{[]float64{1, 2, 3, 4, 5}, 0.5, 3},
+		{[]float64{1, 2, 3, 4, 5}, 1.0, 5},
+		{[]float64{1, 2, 3, 4, 5}, 0.0, 1},
+	}
+
+	for _, c := range cases {
+		actual := Quantile(c.numbers, c.q)
+		if actual != c.expected {
+			t.Errorf("Quantile(%v, %v) == %v, expected %v", c.numbers, c.q, actual, c.expected)
+		}
+	}
+}
